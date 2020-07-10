6937c053dea4e04f05a31c79af307f24f1a49c72
Max Brokman <max.brokman@gmail.com>
2020-07-10 19:03:16 +0100

    add commend about printing db query

--- vice/statistics.py
+++ vice/statistics.py
@@ -4,6 +4,7 @@ def get_job_statistics(job_id):
     conn = _get_postgres_connection()
     cursor = conn.cursor()
 
+    # this is here to help you make sure your database connection is working
     cursor.execute("SELECT * from jobs")
     print(cursor.fetchone())
     return {}
